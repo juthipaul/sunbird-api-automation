309896500981662b778e19b52e320febe2b099ca
Juthika Paul <57178572+juthipaul@users.noreply.github.com>
2022-02-23 20:30:00 +0530

    Update Utils.java

--- src/test/java/resources/Utils.java
+++ src/test/java/resources/Utils.java
@@ -21,7 +21,7 @@ public class Utils {
 	static Properties prop;
 	public static RequestSpecification req;
 	JSONParser parser = new JSONParser();
-	Object obj = parser.parse(new FileReader("src\\test\\java\\resources\\global.properties"));
+	Object obj = parser.parse(new FileReader("/src/test/java/resources/global.properties"));
 	JSONObject jsonObject = (JSONObject) obj;
 	public  String baseuri=(String)jsonObject.get("baseUrl");
 	public  String auth_key=(String)jsonObject.get("authToken");
@@ -54,7 +54,7 @@ public class Utils {
 	public static String getGlobalValue(String key) throws IOException
 	{
 		prop =new Properties();
-		FileInputStream fis =new FileInputStream("src\\test\\java\\resources\\global.properties");
+		FileInputStream fis =new FileInputStream("/src/test/java/resources/global.properties");
 		prop.load(fis);
 		return prop.getProperty(key);
 	
