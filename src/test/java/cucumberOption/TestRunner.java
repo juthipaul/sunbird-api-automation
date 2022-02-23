0bb47ac47e7a23d88a5cc5ee243d96af73103123
Juthika Paul <57178572+juthipaul@users.noreply.github.com>
2022-02-23 20:24:41 +0530

    Update TestRunner.java

--- src/test/java/cucumberOption/TestRunner.java
+++ src/test/java/cucumberOption/TestRunner.java
@@ -5,16 +5,13 @@ import org.junit.runner.RunWith;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
-
-
 @RunWith(Cucumber.class)
 @CucumberOptions
 (
-		features = "src/test/java/features/",
+		features = "src/test/java/features/question.feature",
 		glue= { "stepDefinitions/"},
-		//tags= {"@question"},
+		tags= {"@question"},
 		plugin= {"json:target/jsonReports/cucumber-report.json"}
-		
 )
 public class TestRunner {
 
