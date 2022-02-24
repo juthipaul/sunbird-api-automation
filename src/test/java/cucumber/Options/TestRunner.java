f4284ac2f3b2b4fdfa78b3bb1c0470bc11e8bc38
Juthika Paul <57178572+juthipaul@users.noreply.github.com>
2022-02-24 07:43:17 +0530

    Update TestRunner.java

--- src/test/java/cucumber/Options/TestRunner.java
+++ src/test/java/cucumber/Options/TestRunner.java
@@ -2,8 +2,8 @@ package Options;
 
 import org.junit.runner.RunWith;
 
-import io.cucumber.junit.CucumberOptions;
-import io.cucumber.junit.Cucumber;
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions
